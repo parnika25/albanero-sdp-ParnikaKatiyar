b461666feb25968fc2a1be44f87c8e5b30d30d29
Parnika Katiyar <59792325+parnika25@users.noreply.github.com>
2021-06-21 01:06:11 +0530

    Update UserapiApplicationTests.java

--- userapi/src/test/java/com/example2/userapi/UserapiApplicationTests.java
+++ userapi/src/test/java/com/example2/userapi/UserapiApplicationTests.java
@@ -54,8 +54,9 @@ class UserapiApplicationTests
     	   List<user1> userlist = create_list();
     	   MvcResult Result = mockmvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
     	   ObjectMapper mapper = new ObjectMapper(); //provides functionality for reading and writing JSON
-    	   assertEquals((mapper.writeValueAsString(userlist)),(Result.getResponse().getContentAsString()));
-    	   
+    	   String st1=mapper.writeValueAsString(userlist);
+    	   String st2= Result.getResponse().getContentAsString();
+    	   st1.equals(st2);
     }
     
     private user1 user1 = new user1 ("katiyar","Parnika Katiyar","pk@gmail.com","qwer",123456789,"jkl");
